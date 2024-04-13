db1666580cfe622b88b50d0e86e3431765123d5c
Davide <myearthlydelight@gmail.com>
2024-04-13 14:23:14 +0200

    moved migration to separate script

--- src/lib/supabase/db.ts
+++ src/lib/supabase/db.ts
@@ -1,28 +1,13 @@
 import { drizzle } from "drizzle-orm/postgres-js"
 import postgres from "postgres"
-import { migrate } from "drizzle-orm/postgres-js/migrator"
 import * as dotenv from "dotenv"
-import * as schema from "../../../migrations/schema"
+import * as schema from "./schema"
 
 dotenv.config({ path: ".env" })
 
-if (!process.env.DATABASE_URL) {
-  console.log("🔴 Cannot find database url")
-}
+if (!process.env.DATABASE_URL) throw new Error("Database url cannot be found")
 
-const client = postgres(process.env.DATABASE_URL as string, { max: 1 })
+const client = postgres(process.env.DATABASE_URL || "", { max: 1 })
 const db = drizzle(client, { schema })
-
-const migrateDb = async () => {
-  console.log("🟠 Migrating client")
-  try {
-    await migrate(db, { migrationsFolder: "migrations" })
-    console.log("🟢 Successfully Migrated")
-  } catch (error) {
-    console.log("🔴 Error Migrating Client", error)
-  }
-}
-
-migrateDb()
 
 export default db
